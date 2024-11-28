5e620be75b5db36216e292cadb7ebdfeb4e0f4ac
fajar <if23.fajarpratama@mhs.ubpkarawang.ac.id>
2024-11-28 23:10:35 +0700

    Update login.php

--- backend/login.php
+++ backend/login.php
@@ -1,13 +1,20 @@
 <?php
+// Menyertakan koneksi database
+include('../config/db.php');
+
 if (isset($_POST['email']) || isset($_POST['password'])) { 
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    if ($email == 'fajar@admin.com' && $password == 'admin') {
+    $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // Redirect ke halaman dashboard jika login berhasil
         header('Location: ./../dashboard.php');
         exit();
     } else {
-        // Menampilkan pesan error dengan Bootstrap alert
+        // Tampilkan pesan error jika login gagal
         echo '<div class="alert alert-danger" role="alert">Email atau password salah!</div>';
     }
 }
